d45e946e15b364c5a2e40fbf68f0e6664c83764a
Vanessa <98292181+wvvva@users.noreply.github.com>
2025-07-25 12:19:26 -0400

    Initialize SpringBoot

---
+++ src/main/java/com/springboot/trip_planner_backend/TripPlannerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.trip_planner_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripPlannerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripPlannerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/trip_planner_backend/TripPlannerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.trip_planner_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripPlannerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
